faa64d68f0a015def100042052519b5f1403dcf9
fminimalist-startup-coder <kinshukrajputkr25@gmail.com>
2026-03-22 16:28:35 +0530

    ..

--- api/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Vercel Serverless Entry Point
- * This file exports the Express app for Vercel to handle.
- * Vercel wraps this in a serverless function automatically.
- */
-const app = require('../src/app');
-
-module.exports = app;
